0ec7d88286bc6d5a78da108270951c26fd07b0c1
Maya Mircheva <may4eto@hotmail.com>
2022-04-21 23:13:04 +0200

    added cart

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,30 @@
+class OrderItemsController < ApplicationController
+    def create 
+        @product = Product.find(params[:product_id])
+        @quantity = form_params[:quantity]
+        @current_cart.order_items.create(product: @product, quantity: @quantity)
+        flash[:success] = "You just added #{@quantity} #{@product.title} #{"pin".pluralize(@quantity)} to your cart."
+        redirect_to product_path(@product)
+    end
+
+    def update 
+        @product = Product.find(params[:product_id])
+        @order_item = OrderItem.find(params[:id])
+        @order_item.update(form_params)
+        flash[:success] = "You just added #{@quantity} #{@product.title} #{"pin".pluralize(@quantity)} to your cart."
+        redirect_to product_path(@product)
+    end 
+
+    def destroy 
+        @product = Product.find(params[:product_id])
+        @order_item = OrderItem.find(params[:id])
+        @order_item.delete
+        flash[:success] = "You just deleted #{@product.title} from your cart."
+        redirect_to cart_path
+
+    end
+
+    def form_params 
+        params.require(:order_item).permit(:quantity)
+    end
+end
